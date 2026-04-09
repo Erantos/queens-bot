7e8ba60a9ad37634c39b9364f5a8f41bbfaa87b5
Erantos <magniat.a@gmail.com>
2026-04-09 22:39:05 +0200

    Update to summer menu

--- src/commands/menu.ts
+++ src/commands/menu.ts
@@ -13,8 +13,8 @@ export const menuCmd = {
                 { name: '👨‍🍳 Chef', value: 'Crème de cheddar, tranche de cheddar, bacon, ketchup maison, pickle cornichon, oignons confits' },
                 { name: '🫠 Dirty', value: 'Crème de cheddar, crème de comté, oignons frits, bacon, cheddar, comté, morbier, romarin' },
                 { name: '⚫ Black Pepper', value: 'Sauce aux 3 poivres, oignons frits, thym, morbier, chips de jambon cru, réduction de vinaigre balsamique' },
-                { name: '🎃 Squeenshy', value: 'Crème de potimarron au piment d\'espelette, thym et chorizo grillé, noisettes et poivre de séchuan et oignons frits' },
-                { name: '🏔️ Mountain', value: 'Crème de reblochon, romarin et bacon, raclette fumée, éclats de noix, oignons frits' },
+                { name: '🌴 Estaté', value: 'Pesto de tomates séchées, basilic, olives vertes, chips de jambon cru, tagliatelles de carottes et oignons frits' },
+                { name: '☀️ Verano', value: 'Crème de chorizo, thym et poivre noir, grana padano, éclats d\'amandes, oignons frits' },
                 { name: '🇧‎🇸 Burger semaine', value: 'Une nouvelle recette à découvrir chaque semaine' }
             );
 
@@ -22,12 +22,12 @@ export const menuCmd = {
             .setColor(0xFFA500)
             .setTitle('🥢 Carte Asiatique')
             .addFields(
-                { name: '🐯 Larme du tigre', value: 'Tataki de boeuf, sauce citron vert-piment, pesto de basilic thaï, riz et légumes, riz torréfié et herbes aromatiques' },
-                { name: '🧄 Garlic pepper', value: 'Pop up de poulet panés et sautés "ail et poivre", riz et légumes, herbes aromatiques' },
-                { name: '🍜 Pad thaï', value: 'Vermicelles de riz sautées aux oeufs et crevettes, sauce au tamarin, légumes sautés, herbes aromatiques et cacahuètes (poulet / crevettes / poulet crunchy)' },
-                { name: '🍲 Fondue thaï', value: 'Nouilles de riz, bouillon saté-coco, légumes, cacahuètes, herbes aromatiques (boeuf / crevettes)' },
-                { name: '🍛 Curry rouge', value: 'Pâte de curry maison au piment, crème de coco, riz et légumes, herbes aromatiques, cacahuètes (poulet / crevettes)' },
-                { name: '🐷 Porc au saté', value: 'Porc rôti, mariné au saté, riz et légumes, sauce gingembre - sésame, herbes aromatiques' },
+                { name: '🧄 Garlic pepper', value: 'Pops up de poulet panés, sautés "ail et poivre", riz et légumes, herbes aromatiques, oignons frits' },
+                { name: '🐯 Larme du tigre', value: 'Tataki de boeuf, sauce citron vert-piment, riz torréfié, riz et légumes, herbes aromatiques' },
+                { name: '🍜 Pad thaï', value: 'Vermicelles de riz sautées "oeufs et petites crevettes", sauce au tamarin, légumes, herbes aromatiques, cacahuètes (poulet / crevettes / poulet crunchy)' },
+                { name: '🍛 Curry rouge', value: 'Pâte de curry maison, crème de coco, riz et légumes, herbes aromatiques (poulet / crevettes)' },
+                { name: '🐂 Boeuf aux épices', value: 'Boeuf mariné aux épices, sauce aux champignons parfumés, pickles de bambou, riz et légumes, herbes aromatiques' },
+                { name: '🍱 Bô Bun', value: 'Salade de vermicelles de riz, nems au porc, crudités, herbes aromatiques, cacahuètes (boeuf citronnelle / crevettes / poulet crunchy)' },
                 { name: '🇵‎🇩‎🇲 Plat du moment', value: 'Une recette chaque semaine selon l\'humeur du chef !' },
             )
 
